73e3a9ff39ca3633025eddf65e68aa72c9388eac
samadhan <samadhanbhusnar21@gmail.com>
2026-03-23 11:29:19 +0530

    delete api is added

--- src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -12,16 +12,29 @@ public interface IAccountsService {
 
     /**
      *
+     * This method is used to fetch account details for the customer
+     *
      * @param mobileNumber
      * @return CustomerDto
      */
     CustomerDto fetchAccount(String mobileNumber);
 
     /**
+     * This method is used to update account details for the customer
      *
      * @param customerDto
      * @return boolean
      */
     boolean updateAccount(CustomerDto customerDto);
+
+
+    /**
+     *
+     * This method is used to delete account for the customer
+     *
+     * @param mobileNumber
+     * @return boolean
+     */
+    boolean deleteAccount(String mobileNumber);
 
 }
--- src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
+++ src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -89,4 +89,19 @@ public class AccountServiceImpl implements IAccountsService {
         }
         return isUpdated;
     }
+
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(() -> {
+            return new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber);
+        });
+
+
+        accountsRepository.deleteByCustomerId(customer.getCustomerId());
+        customerRepository.deleteById(customer.getCustomerId());
+        return true;
+
+    }
+
+
 }
